105823d396a28d841e19a44573f841399bc387a7
Aleksandr Krivolap <sashakrivolap@gmail.com>
2019-08-25 21:31:21 +0300

    Update darknet.py

--- python/darknet.py
+++ python/darknet.py
@@ -1,6 +1,10 @@
 from ctypes import *
 import math
 import random
+import cv2
+import time
+import os
+from copy import deepcopy
 
 def sample(probs):
     s = sum(probs)
@@ -42,10 +46,11 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-    
 
+libPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', "libdarknet.so")
+assert(os.path.exists(libPath)), "Cannot find " + libPath
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL(libPath, RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -103,12 +108,20 @@ load_meta = lib.get_metadata
 lib.get_metadata.argtypes = [c_char_p]
 lib.get_metadata.restype = METADATA
 
+load_names = lib.get_names
+lib.get_names.argtypes = [c_char_p]
+lib.get_names.restype = METADATA
+
 load_image = lib.load_image_color
 load_image.argtypes = [c_char_p, c_int, c_int]
 load_image.restype = IMAGE
 
 rgbgr_image = lib.rgbgr_image
 rgbgr_image.argtypes = [IMAGE]
+
+ndarray_image = lib.ndarray_to_image
+ndarray_image.argtypes = [POINTER(c_ubyte), POINTER(c_long), POINTER(c_long)]
+ndarray_image.restype = IMAGE
 
 predict_image = lib.network_predict_image
 predict_image.argtypes = [c_void_p, IMAGE]
@@ -122,35 +135,53 @@ def classify(net, meta, im):
     res = sorted(res, key=lambda x: -x[1])
     return res
 
-def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
-    im = load_image(image, 0, 0)
+def detect(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45):
     num = c_int(0)
     pnum = pointer(num)
     predict_image(net, im)
     dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
+    print("Got detections")
     num = pnum[0]
-    if (nms): do_nms_obj(dets, num, meta.classes, nms);
-
+    if (nms): do_nms_obj(dets, num, meta.classes, nms)
+    print("NMS done")
     res = []
     for j in range(num):
         for i in range(meta.classes):
             if dets[j].prob[i] > 0:
                 b = dets[j].bbox
                 res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
-    res = sorted(res, key=lambda x: -x[1])
+    res = deepcopy(sorted(res, key=lambda x: -x[1]))
+    print("Dealocating image")
     free_image(im)
+    print("Deallocating detections")
     free_detections(dets, num)
     return res
-    
+
+def nparray_to_image(img):
+    data = img.ctypes.data_as(POINTER(c_ubyte))
+    image = ndarray_image(data, img.ctypes.shape, img.ctypes.strides)
+    return image
+
 if __name__ == "__main__":
     #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
+    net = load_net("cfg/yolov3-tiny.cfg", "weights/yolov3-tiny.weights", 0)
     meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
+    t0 = time.time()
+    im1 = load_image("data/dog.jpg", 0, 0)
+    t1 = time.time()
+    r = detect(net, meta, im1)
+    t2 = time.time()
     print(r)
-    
-
+    print("t read: {}\t t detect: {}".format(t1-t0, t2-t1))
+    t0 = time.time()
+    img = cv2.imread("data/dog.jpg")
+    im2 = nparray_to_image(img)
+    t1 = time.time()
+    r = detect(net, meta, im2)
+    t2 = time.time()
+    print(r)
+    print("t read: {}\t t detect: {}".format(t1-t0, t2-t1))
